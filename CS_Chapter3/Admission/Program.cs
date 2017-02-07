b408dbafd1aa0160a49ab57ed4243341d6353ed5
Al-Juwan Williams <syntheticizayoi@gmail.com>
2017-02-06 21:17:27 -0500

    Finished

--- CS_Chapter3/Admission/Program.cs
+++ CS_Chapter3/Admission/Program.cs
@@ -9,10 +9,28 @@ namespace Admission
     {
         static void Main(string[] args)
         {
-            int gpa, score;
+            int score;
+            double gpa;
+            String scan;
             Console.Write("What's your gpa: ");
-            String scan = Console.ReadLine();
+            scan = Console.ReadLine();
+            gpa = Convert.ToDouble(scan);
+            Console.Write("What's what was your score on the admission test: ");
+            scan = Console.ReadLine();
+            score = Convert.ToInt32(scan);
 
+            if (gpa >= 3.0 && score >= 60)
+            {
+                Console.WriteLine("Accepted");
+            }
+            else if (gpa < 3.0 && score >= 80)
+            {
+                Console.WriteLine("Accepted");
+            }
+            else
+            {
+                Console.WriteLine("Reject");
+            }
         }
     }
 }
--- CS_Chapter3/Payroll/Program.cs
+++ CS_Chapter3/Payroll/Program.cs
@@ -9,6 +9,26 @@ namespace Payroll
     {
         static void Main(string[] args)
         {
+            int hours;
+            double pay, week, tax;
+            String scan;
+            Console.Write("Hourly pay: ");
+            scan = Console.ReadLine();
+            pay = Convert.ToDouble(scan);
+            Console.Write("Hours worked: ");
+            scan = Console.ReadLine();
+            hours = Convert.ToInt32(scan);
+            week = hours * pay;
+            if (week > 300)
+            {
+                tax = week - (week * .12);
+                Console.Write("Your pay this week before taxes will be: " + week + "\nAfter taxes it will be: " + tax);
+            }
+            else
+            {
+                tax = week - (week * .10);
+                Console.Write("Your pay this week before taxes will be: " + week + "\nAfter taxes it will be: " + tax);
+            }
         }
     }
 }
